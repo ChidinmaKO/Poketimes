55656e58c3c449f10ac222ef33baa734e95b7e73
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-01-04 04:49:49 +0100

    set up site with react-router

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h3 className="center">Contact Us</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sed fuga provident dicta architecto itaque. Repellat accusantium, cum eaque eos officiis exercitationem velit. Inventore vel non mollitia cupiditate hic quo.</p>
+        </div>
+    )
+}
+
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h3 className='center'>Home</h3>
+            <p>If you want to conquer fear, don't sit home and think about it. Go out and get busy. - <em>Dale Carnegie</em></p>
+        </div>
+    )
+}
+
+
+
+export default Home;
